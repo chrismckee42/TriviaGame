d98a89cb505db893f6d8fbdbaf3bab7ea9ee624d
Christopher McKee <chris.mckee.13@gmail.com>
2019-10-27 18:17:49 -0700

    adds timer

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,6 +11,9 @@ class questionObj {
 }
 
 const questions = []
+var current = 0
+var count
+var time = 99
 for (let i = 0; i < 10; i++) {
     questions[i] = new questionObj("Q" + (i + 1), "A" + (i + 1), "B" + (i + 1), "C" + (i + 1), "D" + (i + 1), 0)
 
@@ -22,16 +25,23 @@ function loadQuestion(n) {
     $("#answers").empty()
     $("#timer").empty()
     $("#question").append($("<h3>").text(q.question))
-    q.answers.forEach(a => {
-        $("#answers").append($("<h5>").text(a).addClass("options"))
+    q.answers.forEach((a, i) => {
+        $("#answers").append($("<div>").text(a).addClass("options").attr("value"))
     });
-    $("#timer").append($("<div>").text("Times Up " + q.correct))
+    $("#timer").text(time + " seconds left " + q.correct)
 
 }
 
-loadQuestion(0)
+function countDown() {
+    time--
+    $("#timer").text(time + " seconds left")
+    if (time <= 0) {
+        // time is up
+    }
+}
 
-$(document).ready()
+loadQuestion(current)
+
 
 $(document).ready(function () {
 
@@ -39,6 +49,14 @@ $(document).ready(function () {
     $(".options").on("click", function () {
 
         console.log($(this).text())
+        if ($(this).text() === questions[current].correct) {
+            console.log("correct", $(this).text(), questions[current].correct)
+        } else {
+            console.log("wrong", $(this).text(), questions[current].answers[questions[current].correct])
+        }
 
     })
+
+    count = setInterval(countDown, 1000)
+
 })
