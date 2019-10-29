f7efc0e6309e4a421a5795f3e6257f7c207ca319
Christopher McKee <chris.mckee.13@gmail.com>
2019-10-28 22:24:16 -0700

    finishes quiz including randomized addition questions

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,11 +13,22 @@ class questionObj {
 const questions = []
 var current = 0
 var count
-var time = 15
-for (let i = 0; i < 10; i++) {
-    questions[i] = new questionObj("Q" + (i + 1), "A" + (i + 1), "B" + (i + 1), "C" + (i + 1), "D" + (i + 1), 0)
+var time = 30
+var right = 0
+var wrong = 0
+var inter = false
+
+function generateQuestions() {
+    for (let i = 0; i < 10; i++) {
+        let a = Math.ceil(Math.random() * 12)
+        let b = Math.ceil(Math.random() * 12)
+        let c = Math.floor(Math.random() * 4)
+        let d = a + b - c
+        questions[i] = new questionObj("What is " + a + " + " + b + "?", d, d + 1, d + 2, d + 3, c)
+    }
 
 }
+generateQuestions()
 
 function loadQuestion(n) {
     const q = questions[n]
@@ -28,30 +39,86 @@ function loadQuestion(n) {
     q.answers.forEach((a, i) => {
         $("#answers").append($("<div>").text(a).addClass("options").val(i))
     });
-    $("#timer").text(time + " seconds left " + q.correct)
+    time = 30
+    $("#timer").text(time + " seconds left ")
 
 }
 
 function intermission(rightAnswer, timeout) {
+    inter = true
     $("#question").empty()
     $("#answers").empty()
     $("#timer").empty()
+    time = 1
+    //console.log(questions[current].answers[questions[current].correct])
     if (timeout) {
         $("#question").text("Time is up!")
         $("#answers").text("The correct answer was " + questions[current].answers[questions[current].correct])
     } else if (rightAnswer) {
         $("#question").text("Correct!")
     } else {
-        $("#question").text("Inorrect!")
+        $("#question").text("Incorrect! The correct answer was " + questions[current].answers[questions[current].correct])
     }
+    current++
+
 }
 
 function countDown() {
     time--
-    $("#timer").text(time + " seconds left")
-    if (time <= 0) {
-        intermission(false, true)
+    //console.log(time, current)
+    if (!inter) {
+        $("#timer").text(time + " seconds left")
+        if (time <= 0) {
+            intermission(false, true)
+        }
+    } else if (time <= 0) {
+        if (current === 10) {
+            gameEnd()
+        } else {
+            inter = false
+            loadQuestion(current)
+            intitalize()
+        }
     }
+}
+
+function intitalize() {
+    $(".options").on("click", function () {
+
+        //console.log($(this).text())
+        if (parseInt($(this).val()) === questions[current].correct) {
+            // console.log("correct", $(this).val(), questions[current].correct)
+            right++
+            intermission(true, false)
+        } else {
+            //console.log("wrong", $(this).val(), questions[current].correct)
+            wrong++
+            intermission(false, false)
+        }
+    })
+}
+
+function gameEnd() {
+    $("#question").empty()
+    $("#answers").empty()
+    $("#timer").empty()
+    $("#question").text("Results")
+    $("#answers").append($("<div>").text("Correct: " + right))
+    $("#answers").append($("<div>").text("Incorrect: " + wrong))
+    $("#timer").text("Click here to play again!")
+    clearInterval(count)
+
+    $("#timer").on("click", function () {
+        current = 0
+        time = 30
+        right = 0
+        wrong = 0
+        inter = false
+        generateQuestions()
+        loadQuestion(current)
+        intitalize()
+        count = setInterval(countDown, 1000)
+    })
 }
 
 loadQuestion(current)
@@ -59,19 +126,7 @@ loadQuestion(current)
 
 $(document).ready(function () {
 
-
-    $(".options").on("click", function () {
-
-        console.log($(this).text())
-        if (parseInt($(this).val()) === questions[current].correct) {
-            console.log("correct", $(this).val(), questions[current].correct)
-            intermission(true, false)
-        } else {
-            console.log("wrong", $(this).val(), questions[current].correct)
-            intermission(false, false)
-        }
-
-    })
+    intitalize()
 
     count = setInterval(countDown, 1000)
 
